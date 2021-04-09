57916ad09aa79f28013dc25667fa958d5e5b3636
guptayash777 <yashg0443@gmail.com>
2021-04-09 20:07:28 +0530

    temp

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
 console.log("hello");
+console.log("yoyo");
+// git version: to see the version
+// git init: to initialise git in folder
+// git remote add origin (url).git : to link local folder with git repository
+// git add . : track the changes made in file
+// git status: tells the changes which are tracked
+// git pull origin master : pull the file from github
+// git push --set-upstream origin master : push the file on github
+// git checkout -b branch_name: changes the branch on which we are working
+// git branch: show the branch on which we are working 
